6a56b2a24697b5c759b306dafaf69926eb7a13dd
Immo Heikkinen <immo.heikkinen@gmail.com>
2024-12-26 08:33:12 +0200

    Day 21 part 2

--- src/day21.clj
+++ src/day21.clj
@@ -6,67 +6,60 @@
   (str/split-lines input))
 
 (defn parse-keypad [keypad]
-  (let [keypad (into {} (for [[i line] (map-indexed vector (str/split-lines keypad))
-                              [j button] (map-indexed vector line)
-                              :when (not= \space button)]
-                          [[i j] button]))]
-    {:keypad keypad
-     :pos (key (first (filter #(= \A (val %)) keypad)))}))
+  (into {} (for [[i line] (map-indexed vector (str/split-lines keypad))
+                 [j button] (map-indexed vector line)
+                 :when (not= \space button)]
+             [[i j] button])))
 
 
 (def numerical-keypad (parse-keypad "789\n456\n123\n 0A"))
 
 (def directional-keypad (parse-keypad " ^A\n<v>"))
 
-(defn apply-press-1 [machine action]
-  (if (string? machine)
-    {:machine (str machine action)}
-    (if (= \A action)
-      {:machine machine :out (get-in machine [:keypad (:pos machine)])}
-      (let [new-pos (mapv + (:pos machine) ({\< [0 -1] \> [0 1] \^ [-1 0] \v [1 0]} action))]
-        (when (get-in machine [:keypad new-pos])
-          {:machine (assoc machine :pos new-pos)})))))
+(defn possible-key-sequences [keypad initial-key target-key]
+  (let [[y1 x1] (key (first (filter #(= initial-key (val %)) keypad)))
+        [y2 x2] (key (first (filter #(= target-key (val %)) keypad)))]
+    (distinct (concat (when (keypad [y2 x1])
+                        [(apply str (concat (repeat (abs (- y2 y1)) (if (< y1 y2) \v \^))
+                                            (repeat (abs (- x2 x1)) (if (< x1 x2) \> \<))
+                                            [\A]))])
+                      (when (keypad [y1 x2])
+                        [(apply str (concat (repeat (abs (- x2 x1)) (if (< x1 x2) \> \<))
+                                            (repeat (abs (- y2 y1)) (if (< y1 y2) \v \^))
+                                            [\A]))])))))
 
-(defn apply-press [state action]
-  (loop [wip state
-         input action
-         result-state []]
-    (if-let [machine (first wip)]
-      (if input
-        (when-let [{new-machine :machine out :out} (apply-press-1 machine input)]
-          (recur (rest wip) out (conj result-state new-machine)))
-        (recur (rest wip) nil (conj result-state machine)))
-      result-state)))
+(declare min-presses-1)
 
-(defn neighbors [state]
-  (keep (partial apply-press state) "<>^vA"))
+(defn min-presses-1* [level directional-keyboard-count keypad initial-key target-key]
+  (if (= directional-keyboard-count level)
+    1
+    (apply min (map (fn [key-sequence]
+                      (reduce + (map (fn [[key1 key2]]
+                                       (min-presses-1 (inc level) directional-keyboard-count directional-keypad key1 key2))
+                                     (partition 2 1 (str "A" key-sequence)))))
+                    (possible-key-sequences keypad initial-key target-key)))))
 
-(defn find-min-button-presses [keypads code]
-  (let [initial-state (conj (vec keypads) "")]
-    (loop [wip {initial-state 0}
-           visited #{}]
-      (when-let [[current distance] (first (sort-by val wip))]
-        (cond
-          (= code (last current))
-          distance
+(def min-presses-1 (memoize min-presses-1*))
 
-          (not (str/starts-with? code (last current)))
-          (recur (dissoc wip current) (conj visited current))
+(defn min-presses [directional-keyboard-count key-sequence]
+  (reduce + (map (fn [[key1 key2]]
+                   (min-presses-1 0 directional-keyboard-count numerical-keypad key1 key2))
+                 (partition 2 1 (str "A" key-sequence)))))
 
-          :else
-          (recur (merge-with min
-                             (dissoc wip current)
-                             (zipmap (neighbors current)
-                                     (repeat (inc distance))))
-                 (conj visited current)))))))
-
-(defn complexity [keypads code]
+(defn complexity [directional-keyboard-count code]
   (* (parse-long (subs code 0 3))
-     (find-min-button-presses keypads code)))
+     (min-presses directional-keyboard-count code)))
 
 ;; Part 1
 
 (defn part1 [input]
   (->> (parse-input input)
-       (map (partial complexity [directional-keypad directional-keypad numerical-keypad]))
+       (map (partial complexity 3))
+       (reduce +)))
+
+;; Part 2
+
+(defn part2 [input]
+  (->> (parse-input input)
+       (map (partial complexity 26))
        (reduce +)))
